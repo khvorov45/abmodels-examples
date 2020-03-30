a06f8332515cb8f1f6f57ef9d6d0d7d75f97a332
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-30 12:43:00 +1100

    added logistic fit example

---
+++ logistic-regression/model-fit-plot/fit-plot.R
@@ -0,0 +1,53 @@
+# Plots the logistic predictions for protection
+
+library(tidyverse)
+library(here)
+
+# Directories used
+lr_fit_dir <- here("logistic-regression", "model-fit")
+lr_fit_plot_dir <- here("logistic-regression", "model-fit-plot")
+
+# Functions ===================================================================
+
+gen_plot <- function(lr_fit) {
+  lr_fit %>%
+    ggplot(aes(logtitre, protection)) +
+    theme_bw() +
+    theme(
+      panel.grid.minor.y = element_blank()
+    ) +
+    coord_cartesian(
+      xlim = c(log(5), log(1280)),
+      ylim = c(0, 1)
+    ) +
+    scale_x_continuous(
+      "Titre",
+      breaks = log(5 * 2^(0:8)),
+      labels = 5 * 2^(0:8),
+      minor_breaks = log(7.5 * 2^(0:8)),
+    ) +
+    scale_y_continuous(
+      "Protection",
+      labels = scales::percent_format()
+    ) +
+    geom_ribbon(
+      aes(ymin = protection_low, ymax = protection_high),
+      alpha = 0.5
+    ) +
+    geom_line()
+}
+
+# Script ======================================================================
+
+# Fitted values
+lr_fit <- read_csv(file.path(lr_fit_dir, "fit.csv"), col_types = cols())
+
+# Plot of protection
+lr_plot <- gen_plot(lr_fit)
+
+# Save plot to the same directory as the script
+ggsave(
+  file.path(lr_fit_plot_dir, "protection.pdf"),
+  lr_plot,
+  width = 12, height = 7.5, units = "cm"
+)
---
+++ logistic-regression/model-fit/fit.R
@@ -0,0 +1,79 @@
+# Fit the logistic model to the simulated data
+
+library(tidyverse)
+library(here)
+
+# Directories used
+lr_data_dir <- here("logistic-regression", "data")
+lr_fit_dir <- here("logistic-regression", "model-fit")
+
+# Functions ===================================================================
+
+#' Reads the simulated lr data
+read_data_sim_lr <- function() {
+  dat_file <- file.path(lr_data_dir, "sim-lr.csv")
+  if (!file.exists(dat_file)) {
+    stop("Run sim.r in data to generate data")
+  }
+  read_csv(
+    dat_file,
+    col_types = cols(
+      status = col_integer()
+    )
+  )
+}
+
+#' Generates predictions for the logistic model
+#'
+#' @param fit_lr Object returned by `glm`
+#' @param newdata Dataframe that holds the covariates used in the formula
+#'   that generated `fit_lr`
+predict_lr <- function(fit_lr, newdata) {
+  pred_vals <- predict(fit_lr, newdata, se = TRUE)
+  newdata %>%
+    mutate(
+      fit = pred_vals$fit,
+      fit_se = pred_vals$se.fit
+    )
+}
+
+#' Generates the protection estimates
+#'
+#' @param predictions Returned by `predict_lr`
+gen_protection <- function(predictions) {
+  invlogit <- function(x) 1 - 1 / (1 + exp(x))
+  predictions %>%
+    mutate(
+      fit_low = fit - qnorm(0.975) * fit_se,
+      fit_high = fit + qnorm(0.975) * fit_se,
+      protection = 1 - invlogit(fit),
+      protection_low = 1 - invlogit(fit_high),
+      protection_high = 1 - invlogit(fit_low)
+    )
+}
+
+# Script ======================================================================
+
+# Simualated data
+data_sim_lr <- read_data_sim_lr()
+
+# Model fit
+lr_fit <- glm(
+  status ~ logtitre,
+  binomial(link = "logit"),
+  data_sim_lr,
+)
+
+# Covariate values for which we want the fitted values
+data_to_fit <- tibble(
+  logtitre = seq(0, 8, length.out = 101)
+)
+
+# Generate predictions manually
+lr_predictions <- predict_lr(lr_fit, data_to_fit)
+
+# Generate protection from the fit
+lr_prot <- gen_protection(lr_predictions)
+
+# Save the fit to the same folder as the script
+write_csv(lr_prot, file.path(lr_fit_dir, "fit.csv"))

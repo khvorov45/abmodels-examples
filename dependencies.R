e67a1af70c7cf473b854e1efe960ddd35e9486c7
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-06 15:00:01 +1000

    added dependency install script

---
+++ dependencies.R
@@ -0,0 +1,7 @@
+if (!"remotes" %in% rownames(installed.packages())) {
+  install.packages("remotes")
+}
+remotes::update_packages(c(
+  "tidyverse", "furrr", "here", "viridis", "extraDistr", "ggrepel", "rsample",
+  "broom"
+))

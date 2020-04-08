c6f9277feb211e5be84f40f4890ae7e16cdc7e18
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-08 13:05:06 +1000

    changed package caching

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,10 @@
 if (!"remotes" %in% rownames(installed.packages())) {
   install.packages("remotes")
 }
-remotes::update_packages(c(
-  "tidyverse", "furrr", "here", "viridis", "extraDistr", "ggrepel", "rsample",
-  "broom", "sclr"
-))
+remotes::update_packages(
+  c(
+    "tidyverse", "furrr", "here", "viridis", "extraDistr", "ggrepel", "rsample",
+    "broom", "sclr"
+  ),
+  upgrade = "always"
+)

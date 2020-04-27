d4f540f4a901880be068b11d02926cc6caedeaa4
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-27 12:07:27 +1000

    updated doc comment

--- data-plot/save_data_plot.R
+++ data-plot/save_data_plot.R
@@ -1,7 +1,7 @@
 #' Saves the plot
 #'
 #' @param plot A `ggplot` to save
-#' @param name Name to save by
+#' @param name Name to save by. Will be prefixed with 'plot-'
 save_data_plot <- function(plot, name) {
   ggsave(
     file.path(data_plot_dir, glue::glue("plot-{name}.pdf")), plot,
